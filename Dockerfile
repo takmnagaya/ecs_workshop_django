f8ed50ddb055dad318252401b2f0a9f21a321e3d
takmnagaya <takmnagaya@gmail.com>
2019-05-21 22:08:35 +0900

    MySQL DBへの接続設定

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
 FROM python:3.7
+
 ENV PYTHONUNBUFFERED 1
+
+RUN apt-get update -qq \
+    && apt-get install -y \
+    mysql-client \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
 RUN mkdir /myapp
 WORKDIR /myapp
+
 ADD requirements.txt /myapp/
 RUN pip install -r requirements.txt
 ADD . /myapp/
--- app/settings.py
+++ app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'HOST': os.getenv('DB_HOST'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': 3306,
     }
 }
 
